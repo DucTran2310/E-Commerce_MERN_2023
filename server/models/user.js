077433e426262239c1ee2c5738d125f699c7df56
DucTran2310 <trananhduc23102000@gmail.com>
2023-07-25 23:15:14 +0700

    hash password and handle error

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose') // Erase if already required
+const bcrypt = require('bcrypt')
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
@@ -63,6 +64,15 @@ var userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModifined('password')) {
+    next()
+  }
+
+  const salt = bcrypt.genSaltSync(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 //Export the model
 module.exports = mongoose.model('User', userSchema);
