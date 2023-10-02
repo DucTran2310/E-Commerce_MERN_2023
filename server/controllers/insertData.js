0f34cb8f5a20b4ec5071c46938937d36c843e33f
DucTran2310 <trananhduc23102000@gmail.com>
2023-10-03 00:45:53 +0700

    Update scrape data and insert data in mongodb

---
+++ server/controllers/insertData.js
@@ -0,0 +1,48 @@
+const Product = require('../models/product')
+const asyncHandler = require('express-async-handler')
+const data = require('../../data/products.json')
+const categoryData = require('../../data/cate-brand.js')
+const ProductCategory = require('../models/productCategory')
+const slugify = require('slugify')
+
+const fn = async (product) => {
+  await Product.create({
+    title: product?.title,
+    slug: slugify(product?.title) + Math.round(Math.random() * 100) + '',
+    description: product?.description,
+    brand: product?.brand,
+    // price: Math.round(Number(product?.price?.match(/\d/g).join('')) / 100),
+    price: product?.price,
+    category: product?.category,
+    quantity: Math.round(Math.random() * 1000),
+    sold: Math.round(Math.random() * 1000),
+    images: product?.images,
+    color: product?.variants?.find(el => el.label === 'Color')?.variants[0]
+  })
+}
+
+const insertProduct = asyncHandler(async (req, res) => {
+  const promises = []
+  for (let product of data) promises.push(fn(product))
+  await Promise.all(promises)
+  return res.json('Create done!!!')
+})
+
+const fn2 = async (cate) => {
+  await ProductCategory.create({
+    title: cate?.cate,
+    brand: cate?.brand
+  })
+}
+
+const insertCategory = asyncHandler(async (req, res) => {
+  const promises = []
+  for (let cate of categoryData) promises.push(fn2(cate))
+  await Promise.all(promises)
+  return res.json('Create category done!!!')
+})
+
+module.exports = {
+  insertProduct,
+  insertCategory
+}
