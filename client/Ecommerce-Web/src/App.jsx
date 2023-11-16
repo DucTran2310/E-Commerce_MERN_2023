7ea9f9c9608f2b948dc1cbe7b824037774aecd49
DucTran <trananhduc23102000@gmail.com>
2023-11-16 21:54:09 +0700

    Create UI list products

--- client/Ecommerce-Web/src/App.jsx
+++ client/Ecommerce-Web/src/App.jsx
@@ -26,10 +26,10 @@ function App() {
         <Route path={path.PUBLIC} element={<Public />} >
           <Route path={path.HOME} element={<Home />} />
           <Route path={path.BLOGS} element={<Blogs />} />
-          <Route path={path.PRODUCTS} element={<Products />} />
           <Route path={path.DETAIL_PRODUCT__CATEGORY__PID__TITLE} element={<DetailProduct />} />
           <Route path={path.FAQ} element={<FAQ />} />
           <Route path={path.OUR_SERVICES} element={<Services />} />
+          <Route path={path.PRODUCTS} element={<Products />} />
         </Route>
         <Route path={path.RESET_PASSWORD} element={<ResetPassword />} />
         <Route path={path.FINAL_REGISTER} element={<FinalRegister />} />
