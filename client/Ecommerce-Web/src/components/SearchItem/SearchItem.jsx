ab2e7c122fd689bae3d86091d76a3cef3fde6f33
DucTran2310 <trananhduc23102000@gmail.com>
2024-01-04 22:19:58 +0700

    Create UI filter product

--- client/Ecommerce-Web/src/components/SearchItem/SearchItem.jsx
+++ client/Ecommerce-Web/src/components/SearchItem/SearchItem.jsx
@@ -2,6 +2,8 @@ import { memo, useEffect, useState } from 'react'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
 import { colors } from '~/utils/constants'
 import { createSearchParams, useNavigate, useParams } from 'react-router-dom'
+import { apiGetProducts } from '~/apis/product'
+import { formatMoney } from '~/utils/helpers'
 
 const SearchItem = (props) => {
 
@@ -13,14 +15,16 @@ const SearchItem = (props) => {
   } = props
 
   const navigate = useNavigate()
-  const {category} = useParams()
+  const { category } = useParams()
 
   const [selected, setSelected] = useState([])
+  const [bestPrice, setBestPrice] = useState(null)
+  const [price, setPrice] = useState([0, 0])
 
   useEffect(() => {
-    if(selected.length > 0) {
+    if (selected.length > 0) {
       navigate({
-        pathname:  `/${category}`,
+        pathname: `/${category}`,
         search: createSearchParams({
           color: selected.join(',')
         }).toString()
@@ -29,6 +33,30 @@ const SearchItem = (props) => {
       navigate(`/${category}`)
     }
   }, [selected])
+
+  const fetchBestPriceProduct = async () => {
+    const response = await apiGetProducts({ sort: '-price', limit: 1 })
+    if (!response.error) {
+      setBestPrice(formatMoney(response.object[0]?.price))
+    }
+  }
+
+  useEffect(() => {
+    if (type === 'input') fetchBestPriceProduct()
+  }, [type])
+
+  useEffect(() => {
+    // const validPrice = price.filter(el => +el > 0)
+
+    // if (price[0] > 0) {
+    //   navigate({
+    //     pathname: `/${category}`,
+    //     search: createSearchParams(price).toString()
+    //   })
+    // } else {
+    //   navigate(`/${category}`)
+    // }
+  }, [price])
 
   const handleSelect = (e) => {
     const alreadyEl = selected.find(el => el === e.target.value)
@@ -82,6 +110,47 @@ const SearchItem = (props) => {
                     </div>
                   ))
                 }
+              </div>
+            </div>
+          }
+          {
+            type === 'input' &&
+            <div onClick={e => e.stopPropagation()} >
+              <div className='p-4 items-center flex justify-between gap-8 border-b'>
+                <span className='whitespace-nowrap'>
+                  {`The highest price is ${bestPrice} VND`}
+                </span>
+                <span
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    setSelected([])
+                  }}
+                  className='underline cursor-pointer hover:text-main'
+                >
+                  Reset
+                </span>
+              </div>
+              <div className='flex items-center p-2 gap-2'>
+                <div className='flex items-center gap-2'>
+                  <label htmlFor='from'>From</label>
+                  <input
+                    className='form-input'
+                    type='number'
+                    id='from'
+                    value={price[0]}
+                    onChange={e => setPrice(prev => prev.map((el, index) => index === 0 ? e.target.value : el))}
+                  />
+                </div>
+                <div className='flex items-center gap-2'>
+                  <label htmlFor='from'>To</label>
+                  <input
+                    className='form-input'
+                    type='number'
+                    id='to'
+                    value={price[1]}
+                    onChange={e => setPrice(prev => prev.map((el, index) => index === 1 ? e.target.value : el))}
+                  />
+                </div>
               </div>
             </div>
           }
---
+++ client/Ecommerce-Web/src/hooks/useDebounce.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+const useDebounce = (value, ms) => {
+
+  const [debounceValue, setDebounceValue] = useState('')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDebounceValue(value)
+    }, ms)
+  }, [value, ms])
+
+  return debounceValue
+}
+
+export default useDebounce
+
+// Khi mà nhập thay đổi giá thì sẽ gọi API
+// Vấn đề: gọi API liên tục theo mỗi lượt nhập
+// resolve: chỉ call API khi mà người dùng nhập xong
+// Thời gian onChange
+
+// Tách price thành 2 phần
+// 1. biến để phục vụ UI, gõ tới đâu thì lưu tới đó => UI render
+// 2. biến dùng call API => setTimeout => biến sẽ gán sau 1 khoảng thời gian
+
